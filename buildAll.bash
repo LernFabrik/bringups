700f2018046eba9adac70534e6b4e65c3ee2eb51
lentychang <lixis630@gmail.com>
2019-05-03 14:44:52 +0200

    melodic init

--- buildAll.bash
+++ buildAll.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-IMAGE_NAME=ros-melodic
-docker build -t $IMAGE_NAME:base -f Dockerfile.base .
-docker build --build-arg DOCKER_BASE_IMAGE=$IMAGE_NAME:base -t $IMAGE_NAME:desktop -f Dockerfile.desktop .
-
+IMAGE_NAME=iwtros-melodic
+BRINGUPS_PATH=$PWD
+cd ../../../
+docker build -t $IMAGE_NAME:base -f $BRINGUPS_PATH/Dockerfile.base .
+docker build --build-arg DOCKER_BASE_IMAGE=$IMAGE_NAME:base -t $IMAGE_NAME:desktop -f $BRINUPS_PATH/Dockerfile.desktop .
+docker build --build-arg DOCKER_BASE_IMAGE=$IMAGE_NAME:desktop -t $IMAGE_NAME:dev -f $BRINGUPS_PATH/Dockerfile.iwtros .
