2057e8a144933a1981c42788e854334eb231825a
lentychang <lixis630@gmail.com>
2019-05-03 12:40:49 +0200

    build success

--- buildAll.bash
+++ buildAll.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-IMAGE_NAME=ros-melodic
+IMAGE_NAME=iwtros-kinetic
 docker build -t $IMAGE_NAME:base -f Dockerfile.base .
 docker build --build-arg DOCKER_BASE_IMAGE=$IMAGE_NAME:base -t $IMAGE_NAME:desktop -f Dockerfile.desktop .
+docker build --build-arg DOCKER_BASE_IMAGE=$IMAGE_NAME:desktop -t $IMAGE_NAME:dev -f Dockerfile.iwtros .
 
